ffdafdeb941ad665a47a3fc931793975abb5e58f
Zead-Alshu22 <zead.alshukairi@planen-bauen40.de>
2026-07-14 09:32:51 +0200

    adding construct-x dec extension instead of tx-dcp to con-x control plane

--- edc-controlplane/edc-controlplane-construct-x/con-x-controlplane-postgresql-hashicorp-vault/build.gradle.kts
+++ edc-controlplane/edc-controlplane-construct-x/con-x-controlplane-postgresql-hashicorp-vault/build.gradle.kts
@@ -36,7 +36,7 @@ dependencies {
     implementation("org.eclipse.tractusx.edc:agreements:$txVersion")
     implementation("org.eclipse.tractusx.edc:retirement-evaluation-store-sql:$txVersion")
     implementation("org.eclipse.tractusx.edc:control-plane-migration:$txVersion")
-    implementation("org.eclipse.tractusx.edc:tx-dcp:$txVersion")
+    implementation(project(":edc-extensions:dcp:con-x-dcp"))
     implementation(project(":edc-extensions:constructx-policy:policy-evaluation-registry"))
 }
 
